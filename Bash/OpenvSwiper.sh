1e515ba5b593716a3cea616b554c8b75bd064b27
Philippe <philippe.ploegaert@openvstorage.com>
2016-06-22 10:19:08 +0200

    Remove -disabled entries too

--- Bash/OpenvSwiper.sh
+++ Bash/OpenvSwiper.sh
@@ -43,7 +43,7 @@ rm -rf /etc/avahi/services/*
 #kill remaining ovs services and log files (fix for 2.4 webapps problem)
 # rm -rf /etc/init/ovs-*
 # rm -rf /etc/init/alba-*
-for f in $(ls /etc/init/ovs-*.conf /etc/init/alba-*.conf /etc/init/asd-*.conf)
+for f in $(ls /etc/init/ovs-* /etc/init/alba-* /etc/init/asd-*)
 do
   rm ${f}
 done
